19df7a4530b7dd4da73a8a6e8f42f4cfbd68b218
razilsyah <razilsyah5@gmail.com>
2020-11-18 00:07:37 +0700

    menambahkan file casting.py

---
+++ casting.py
@@ -0,0 +1,16 @@
+# casting ,merubah tipe ke tipe lain
+# tipe data = string ,integer,float,boolean
+
+data_int = 9
+print("data:", data_int, " bertipe:", type(data_int))
+
+# dari tipe integer di rubah menjadi boolean
+data_bool = bool(data_int)
+print("data:", data_bool, " bertipe:", type(data_bool))
+
+# dari type boolean di rubah ke float
+data_float = float(data_int)
+print("data:", data_float, " bertipe:", type(data_float))
+
+
+# CONTOH SEDERHANA DARI CASTING
