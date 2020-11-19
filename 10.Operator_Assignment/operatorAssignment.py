998648e8031ca7368382c317734b28ed9a1bce8c
razilsyah <razilsyah5@gmail.com>
2020-11-19 22:58:18 +0700

    operator assignment

---
+++ 10.Operator_Assignment/operatorAssignment.py
@@ -0,0 +1,51 @@
+# Operator assignment adalah operasi penyingkatan
+
+# OPERASI ASSIGNMENT PENJUMLAHAN
+print("========OPERASI ASSIGNMENT PENJUMLAHAN======")
+
+a = 5
+print("nilai a =",a)
+a += 5 # jadi maksudnya a = a + 5
+print("nilai a += 5,menjadi :",a)
+
+
+# OPERASI ASSIGNMENT PENGURANGAN
+print("========OPERASI ASSIGNMENT PENGURANGAN======")
+a -= 5
+print("nilai a -= 5,menjadi :",a)
+
+
+# OPERASI ASSIGNMENT PERKALIAN
+print("========OPERASI ASSIGNMENT PERKALIAN======")
+a *= 5
+print("nilai a *= 5,menjadi :",a)
+
+
+# OPERASI ASSIGNMENT PEMBAGIAN
+print("========OPERASI ASSIGNMENT PEMBAGIAN======")
+a /= 5
+print("nilai a /= 5,menjadi :",a)
+
+
+# OPERASI ASSIGNMENT MODULUS
+print("========OPERASI ASSIGNMENT MODULUS======")
+a %= 2
+print("nilai a &= 2,menjadi :",a)
+
+
+# OPERASI ASSIGNMENT FLOOR DIVISION
+print("========OPERASI ASSIGNMENT DIVISION======")
+b = 9
+print("angka b =",b)
+b //= 2
+print("nilai b //= 2,menjadi :",b)
+
+
+
+# OPERASI ASSIGNMENT BITWISE
+print("========OPERASI ASSIGNMENT BITWISE=========")
+
+d = True
+print("nilai d = ",d)
+
+
