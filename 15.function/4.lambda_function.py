a8659b8bfe44eb64b6d457af300464f0ab888c73
razilsyah <razilsyah5@gmail.com>
2020-11-22 03:13:12 +0700

    modified function lambda dan menambahkan file scope global dan local

---
+++ 15.function/4.lambda_function.py
@@ -0,0 +1,15 @@
+# function biasa
+
+def tambah(a,b):
+    c = a + b
+    return c
+
+hasil = tambah(5,7)
+print(hasil)
+
+
+# anonymous lambda
+
+kali = lambda argument1,argument2: argument1*argument2
+hasil_kali = kali(8,9)
+print(hasil_kali)
