9f5716b3e098c617f93552b40b6abd235527e4f7
razilsyah <razilsyah5@gmail.com>
2020-11-21 23:31:39 +0700

    menambahkan file studyCase_perulangan.py

---
+++ 14.pengulangan/studyCase_perulangan.py
@@ -0,0 +1,12 @@
+# 1.buatlah program untuk menampilkan angka 1 - 5
+
+for i in range(1,5):
+    print(i)
+
+# 2. buatlah program untuk menampilkan pola
+input = int(input("masukan jumlah bintang :"))
+
+for input in range(1,input+1):
+    print("*"*input)
+
+
--- studyCase.py
+++ studyCase.py
@@ -1,7 +1,9 @@
+# input variable
 gaji_poko = int(input("masukan gaji poko anda :"))
 status_menikah = bool(int(input("masukan status menikah :")))
 golongan = input("masukan golongan anda :")
 
+# proses
 if status_menikah is True:
     tunjangan_kelaurga = gaji_poko * 0.25
     print("status meikah")
@@ -15,6 +17,7 @@ elif golongan == "b" or golongan == "A":
 else:
     tunjangan_jabatan = gaji_poko * 0.15
 
+# output
 gaji_pegawai = gaji_poko + tunjangan_kelaurga+ tunjangan_jabatan
 print(tunjangan_jabatan)
 print(gaji_pegawai)
