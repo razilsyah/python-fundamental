de3a4b6eb876ec9074cd713c3d0c3ed74aaa873f
razilsyah <razilsyah5@gmail.com>
2020-11-25 15:23:26 +0700

    belajar struktur data tuple

---
+++ 19.strukturData_tuple/struktureData_tuple.py
@@ -0,0 +1,32 @@
+# perbandingan struktur data list dan tuple
+# tuple di mulai dengan ()
+
+# list dan tuple
+data_list = [1,2,3,4,5,6]
+data_tuple = (1,2,3,4,5,6)
+print(data_list)
+print(data_tuple)
+
+data_list.append(9)
+print(data_list)
+
+# data_tuple.append() => data tuple tidak bisa di tambah datanya ,akan terjadi error
+
+
+
+# cara mengetahui apa saja yang bisa di lakukan oleh struktur data
+print("========= method data list ==========")
+
+method_list = dir(data_list)
+print(method_list)
+
+print("========= method data tuple ===========")
+
+method_tuple = dir(data_tuple)
+print(method_tuple)
+
+
+
+
+
+
