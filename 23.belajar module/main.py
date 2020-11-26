be544cf3da93999de070d9c701dc3dd93013caee
razilsyah <razilsyah5@gmail.com>
2020-11-26 16:55:31 +0700

    belajar module

---
+++ 23.belajar module/main.py
@@ -0,0 +1,20 @@
+### 1
+# import modul_matematika
+# modul_matematika.tambah(5,6)
+
+### 2
+# import modul_matematika as mtk
+# mtk.tambah(10,11)
+
+### 3
+# from modul_matematika import tambah,kurang
+#
+# tambah(100,22)
+# kurang(20,20)
+
+
+### 4
+from modul_matematika import *
+
+tambah(1111,2222)
+kurang(1234,123)
---
+++ 23.belajar module/modul_matematika.py
@@ -0,0 +1,8 @@
+
+def tambah(a,b):
+    print("ini modul di file modul_matematika")
+    print(f"{a} + {b} = {a+b}")
+
+def kurang(a,b):
+    print("ini modul di file modul_matematika")
+    print(f"{a} - {b} = {a-b}")
