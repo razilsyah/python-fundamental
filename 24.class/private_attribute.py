d861728ae2d10d293ca36c8f7b5194852b412c9a
razilsyah <razilsyah5@gmail.com>
2020-11-27 15:43:51 +0700

    belajar private attribut

---
+++ 24.class/private_attribute.py
@@ -0,0 +1,15 @@
+class mobil():
+
+    def __init__(self,kondisi):
+        self.Nama = kondisi
+
+    def belajar(self,lokasi): #fungsi dalam class disebut method
+        print(self.Nama,"sedang tes drive",lokasi)
+
+
+# main programnya
+
+classMobil = mobil("toyota")
+print(classMobil.Nama)
+
+classMobil.belajar("di sumedang"  )
