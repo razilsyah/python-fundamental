8b564790ace30888615f2a0b402606300659013b
razilsyah <razilsyah5@gmail.com>
2020-11-27 10:48:55 +0700

    belajar class dan method

---
+++ 24.class/class.py
@@ -0,0 +1,13 @@
+# class
+class mobil():
+    nama = "nama" # atribut
+
+    def belajar(self,lokasi): #fungsi dalam class disebut method
+        print(self.nama,"sedang tes drive",lokasi)
+
+
+# main programnya
+
+namaMobil = mobil()
+namaMobil.nama = "daihatsu"
+namaMobil.belajar("di sumarecon")
